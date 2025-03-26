1f7c633c7100e8f6f2f58fcda4008b8671cd7537
Gheorghe Cojocaru <gheorghe.cojocaru@tui.co.uk>
2025-03-26 15:34:07 +0200

    Initial Commit

---
+++ TripTribeTests/TripTribeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripTribeTests.swift
+//  TripTribeTests
+//
+//  Created by Gheorghe Cojocaru on 26.03.2025.
+//
+
+import Testing
+@testable import TripTribe
+
+struct TripTribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
